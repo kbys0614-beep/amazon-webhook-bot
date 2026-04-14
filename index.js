7b1f175c7a61ef5d33914a7ad7b864da4ec744e7
kbys0614-beep <kbys0614@gmail.com>
2026-04-14 13:46:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-
 app.use(express.json());
+
+const { startMonitor, getStatus } = require('./monitor/stock-monitor');
 
 app.post("/webhook", (req, res) => {
   console.log("受信:", req.body);
   res.send("ok");
 });
+
+app.get('/monitor/status', (req, res) => res.json(getStatus()));
+
+startMonitor();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("server running"));
